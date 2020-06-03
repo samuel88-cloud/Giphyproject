107cfee9fb6900d191f7a8f04e7043864cbb2f64
samuel stephen <samuel8stephen@gmail.com>
2020-06-03 10:31:03 +0530

    integrated quill and GIF search

--- giphyapp/urls.py
+++ giphyapp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.quill),
-    path('index/',views.index,name='index'),
+    path('',views.quill,name='quill'),
+    path('searchindex/',views.searchindex,name='searchindex'),
     path('search/',views.search,name='search'),
     path('next/',views.next,name='next'),
-
+    path('search/addtoquill/<path:image>/',views.addtoquill,name='addtoquill'),
+    path('next/addtoquill/<path:image>/',views.addtoquill),
 
 ]
--- giphyapp/views.py
+++ giphyapp/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 import urllib.request,json
 
-# Create your views here.
+# updated views
 from django.http import HttpResponse
 def quill(request):
-    return render(request,'quill1.html')
+    return render(request,'quill.html')
 
-def addtoquill(request):
-    return render(request,'quill1.html')
+def addtoquill(request,image):
+    print('inside addtoquill')
+    imageurl=image
+    print(imageurl)
+    return render(request,'quill.html',{'imageurl':imageurl})
 
-def index(request):
-    button=0
-    return render(request,'index.html')
+def searchindex(request):
+    print('inside index')
+    return render(request,'searchindex.html')
 
 def search(request):
-    button=1
+    print('inside search')
+
     searchstring=request.POST['imagetosearch']
+    searchstring="".join(searchstring.split()) 
     offset=0
     print(searchstring)
-    url="http://api.giphy.com/v1/gifs/search?q="+str(searchstring)+"&api_key=8sj5guiBo7DEkn4tHTXEp8i7wnauKrjZ&limit=8&offset="+str(offset)
+    url="http://api.giphy.com/v1/gifs/search?q="+str(searchstring)+"&api_key=8sj5guiBo7DEkn4tHTXEp8i7wnauKrjZ&limit=18&offset="+str(offset)
     print(url)
     #data=json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5").read())
 
@@ -29,15 +34,14 @@ def search(request):
     for i in data1:
         print(i['images']['480w_still']['url'])
         imagedata.append(i['images']['preview_gif']['url'])
-    offset=offset+8
-    return render(request,'index.html',{'imagedata':imagedata,'searchstring':searchstring,'offset':offset})
+    return render(request,'searchindex.html',{'imagedata':imagedata,'searchstring':searchstring,'offset':offset})
 
 def next(request):
     searchstring=request.POST['searchstringfrompage']
-    offset=int(request.POST['offsetfrompage'])+8
+    offset=int(request.POST['offsetfrompage'])+18
     print(searchstring)
     print(offset)
-    url="http://api.giphy.com/v1/gifs/search?q="+str(searchstring)+"&api_key=8sj5guiBo7DEkn4tHTXEp8i7wnauKrjZ&limit=8&offset="+str(offset)
+    url="http://api.giphy.com/v1/gifs/search?q="+str(searchstring)+"&api_key=8sj5guiBo7DEkn4tHTXEp8i7wnauKrjZ&limit=18&offset="+str(offset)
     print(url)
     #data=json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5").read())
 
@@ -49,4 +53,4 @@ def next(request):
         print(i['images']['480w_still']['url'])
         imagedata.append(i['images']['preview_gif']['url'])
 
-    return render(request,'index.html',{'imagedata':imagedata,'searchstring':searchstring,'offset':offset})
+    return render(request,'searchindex.html',{'imagedata':imagedata,'searchstring':searchstring,'offset':offset})
